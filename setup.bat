182d74936bbaa6a21f15178e3baf376cdfa791d7
黃文龍 <wlhunag@gmail.com>
2014-01-04 22:44:51 +0800

    not making one installer file

--- setup.bat
+++ setup.bat
@@ -2,11 +2,11 @@
 @setup_cxfreeze.py build
 @echo 接下來要壓縮亂數程式
 
-"C:\Program Files\7-Zip\7z.exe" a -tzip G:\Installer\亂數程式.zip build\exe.win32-2.7
+"C:\Program Files\7-Zip\7z.exe" a -tzip build\亂數程式.zip build\exe.win32-2.7
 @echo 接下來要製作安裝檔案
 
 @echo
-"C:\Program Files (x86)\Inno Setup 5\Compil32.exe" /cc "C:\Users\Aaron\Documents\random_choice_portable_Ramdisk.iss"
+rem "C:\Program Files (x86)\Inno Setup 5\Compil32.exe" /cc "C:\Users\Aaron\Documents\random_choice_portable_Ramdisk.iss"
 
 @echo
 @echo 接下來要連線到 ftp
